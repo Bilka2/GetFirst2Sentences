223a80405c0980aa783ab01f4b90e2c7b9e6e8fa
Bilka <Bilka2@users.noreply.github.com>
2017-10-30 21:16:53 +0100

    understanding of classes has slightly increased
    root all the things (?)

--- GetFirst2Sentences.php
+++ GetFirst2Sentences.php
@@ -12,10 +12,9 @@ class GetFirst2Sentences
     public static function getMagicWord(\Parser $parser, array $cache, $magicWordId, &$ret)
     {
         if ($magicWordId == 'getfirst2sentences') {
-			$page = WikiPage::factory($parser->getTitle())
-            //$article = new \Article($parser->getTitle());
-            $content = $page->getRevision()->getContent( Revision::RAW );
-			$ret = ContentHandler::getContentText( $content );
+			$page = \WikiPage::factory($parser->getTitle());
+            $content = $page->getRevision()->getContent( \Revision::RAW );
+			$ret = \ContentHandler::getContentText( $content );
         }
 
         return true;
