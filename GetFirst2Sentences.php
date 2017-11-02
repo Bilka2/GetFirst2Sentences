ee94ad775b1d5ad649391e871cf69808a05c8794
Bilka <Bilka2@users.noreply.github.com>
2017-11-02 21:23:53 +0100

    Exclude ''this article is.. from results

--- GetFirst2Sentences.php
+++ GetFirst2Sentences.php
@@ -15,12 +15,16 @@ class GetFirst2Sentences
 			$page = \WikiPage::factory($parser->getTitle());
             $content = $page->getRevision()->getContent( \Revision::RAW );
 			$text = \ContentHandler::getContentText( $content );
-			preg_match('/^[^{\n][^.]+.[^\n.]+./m', $text, $matches);
+			//we might catch the ''this article is about... stuff that's on some pages, but we don't want that
+			if (preg_match('/^\'\'(this (article|page)).+\'\'$/im', $text, $match)) {
+				$text = substr($text, strlen($match[0]));
+			}
+			preg_match('/^[^{\n=<][^\.]+\.([^\n\.]+\.)?/m', $text, $matches);
 			//remove links
-			$noInterwiki = preg_replace('/\[\[|\]\]/', '', preg_replace('/\[\[[^\|\]]+\|/', '', $matches[0]) );
-			$noLinks = preg_replace('/\[|\]/', '', preg_replace('/\[[^\s\]]*( |\])/', '', $noInterwiki) );
-			if (strlen($noLinks) > 300) {
-				$noLinks = substr($noLinks, 0, strpos($noLinks, ' ', 240)+1) . "...";
+			$noInterwiki = preg_replace('/\[\[|\]\]/', '', preg_replace('/\[\[[^\|\]]+\|/', '', $matches[0]));
+			$noLinks = preg_replace('/\[|\]/', '', preg_replace('/\[[^\s\]]*( |\])/', '', $noInterwiki));
+			if (strlen($noLinks) > 280) {
+				$noLinks = substr($noLinks, 0, strpos($noLinks, ' ', 230)+1) . "...";
 			}
 			$ret = $noLinks;
         }
